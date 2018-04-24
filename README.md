3f0b0e3f167b644724da48f6f1d1df017d0f638a
Lewis Watson <lewwwiswatson@gmail.com>
2018-04-24 08:37:03 +0100

    :memo: Update README

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# iToC
+# Psychological Study for Kingston University
+
+This is an app which was used in a study into how interaction with our surroundings affects our ability to solve a puzzle. The participant is given a puzzle to solve involving moving tokens around on a grid (NO SPOILERS -- the app will be being used again). One group were given a real life representation of the grid and tokens were and able to move the tokens in the real world, one group were able to move them on the screen, one group could view the puzzle on the screen but not move the tokens. They were given 30 seconds thinking / attempting time, and 45 seconds to complete their answers, if incorrect or they ran out of time the puzzle was reset up to a maximum of 20 times.
+
+In the second run some changes have been requested, in order to filter out discrepancies such as the amount of time it takes a participant to fill in their answers, and whether the participant is able to distinguish between rows and columns (No really!). This means the participant will now tell the researcher their answers and the researcher will now fill in the form for them, and the time it taken to fill in the answers will no longer be timed and recorded. The study will now also be run with children, and the instructions given more clearly and more in-depth for both children and adults.
+
+### Current client requests
+
+- Add instructions pages and end the app thanking the participant if the do not wish to continue.
+- Change the Age and Sex form so that Sex is a dropdown with only 'Male', 'Female' or 'Prefer not to Say'.
+- Board and entry form - the countdown for entry time is no longer required.
+- Change the 'Incorrect' screen to make it clear that although some of the entered token moves may have been correct, the puzzle was not solved.
+- For the high-interactivity (when participants are able to move the tokens on the screen, automatically reset the board / grid every 30 seconds.
+
+### Current coding wish-list
+
+- Add feature and unit testing to make future changes easier to implement.
+- DRY the current code as much as possible.
+- Apply SOLID principles as far as possible.
+- Rename the different versions in terms of interactivity level rather than v1, v2 etc.
+
+To run the app:
+```
+git clone git@github.com:l3w15/iToC.git
+open iToc_v1/index.html
+```
+
+This can be repeated for iToC_v2 and iToC_v3 to see the different versions. I also made what I thought was a better CSS in iToC_v2_backgrounds but the client thought this might be too distracting!
---
+++ iToC_v3/assets/js/app.js
@@ -0,0 +1,138 @@
+
+var unsortedAnswer = [],      unsortedRecord = [],
+    answer = [],              answerRecord = [],
+    remainingSecs,            data,
+    participantInfo = [],     participantNum,
+    interval,                 counter = 0,
+    part = $("#partInfo .form-control"),
+    letters = $(".form-control[name=letter]"),
+    rows = $(".form-control[name=row]"),
+    columns = $(".form-control[name=column]"),
+    correctAnswer = ["4E", "7B", "7H", "Q", "R", "W"],
+    gong = new Audio("assets/gong.mp3");
+
+
+function timer(secs, id, func) {
+  var reset = secs;
+  remainingSecs = secs;
+  interval = setInterval(function() {
+     secs--
+     if (secs >= 10) {
+       $(id).text(secs);
+     } else {
+       $(id).text("0" + secs);
+     }
+     if (secs == -1) {
+        func();
+        secs = reset;
+     }
+  }, 1000);
+}
+
+function start() {
+  $("#form").trigger("reset");
+  $("#cover").toggleClass("hide");
+  $("#puzzle").toggleClass("hide");
+  $("#formCover").toggleClass("partCover hide");
+  $("#partInfo").addClass("hide");
+  $("#instructions").html("Moving only 3 tokens and moving only to the grey squares you must make the arrow point upwards. You have 30 seconds to study the puzzle and choose 3 tokens to solve the puzzle. Decide on the new position for each token. Once the 30 seconds are up, you have 45 seconds to fill in the form below. For each token, enter the letter corresponding to the token, then the co-ordinates of the row and column for the new position.")
+  answer = [];
+  unsortedAnswer = [];
+  timer(30, "#cd1 span", ready);
+
+}
+
+function ready() {
+  gong.play();
+  $("#formCover").toggleClass("partCover hide");
+  $("#timerTwoDisplay").toggleClass("hide");
+  $("#puzzleCover").toggleClass("partCover");
+  clearInterval(interval);
+  unsortedAnswer.push("Ready at " + remainingSecs + "s");
+  $("#cd1 span").text("30");
+  timer(45, "#cd2 span", submit);
+}
+
+function submit() {
+  gong.play();
+  $("#cover").toggleClass("hide");
+  $("#puzzle").toggleClass("hide");
+  $("#puzzleCover").toggleClass("partCover");
+  $("#timerTwoDisplay").toggleClass("hide");
+  clearInterval(interval);
+  unsortedAnswer.push("Submit at " + remainingSecs + "s");
+  $("#cd2 span").text("45");
+  for(var i = 0; i < letters.length; i++){
+    unsortedAnswer.push(letters[i].value.toUpperCase(), rows[i].value + columns[i].value.toUpperCase())
+    answer.push(letters[i].value.toUpperCase(), rows[i].value + columns[i].value.toUpperCase());
+  }
+  unsortedRecord.push(unsortedAnswer);
+  answer.sort();
+  answerRecord.push(answer);
+  if (answer.toString() === correctAnswer.toString()) {
+    $("#cover-btn").html("<h4>CORRECT!</h4>");
+    unsortedRecord.push("Success!");
+    $("#thanks-btn").toggleClass("hide");
+  } else if (counter == 20) {
+    $("#cover-btn").html("<h4>END OF SESSION</h4>");
+    $("#thanks-btn").toggleClass("hide");
+    unsortedRecord.push("Fail!");
+  } else {
+    $("#cover-btn").html("<h4>Incorrect - Click to try again</h4>");
+  }
+}
+
+function exportFile()
+    {
+        var data = [unsortedRecord, participantInfo];
+        //var data = [[1,2,3,4,5],[11,22,33,44,55],[111,222,333,444,555],[1111,2222,3333,4444,5555]];
+        var keys = ['"Times and Form Entry"', '"Participant Info"'];
+
+        var convertToCSV = function(data, keys) {
+            var orderedData = [];
+            for (var i = 0, iLen = data.length; i < iLen; i++) {
+                temp = data[i];
+                for (var j = 0, jLen = temp.length; j < jLen; j++) {
+
+                    quotes = ['"'+temp[j]+'"'];
+                    if (!orderedData[j]) {
+                        orderedData.push([quotes]);
+                    } else {
+                        orderedData[j].push(quotes);
+                    }
+                }
+            }
+            return keys.join(',') + '\r\n' + orderedData.join('\r\n');
+        }
+
+
+        var str = convertToCSV(data, keys);
+
+        var blob = new Blob([str], {type: "text/plain;charset=utf-8"});
+        saveAs(blob, [participantNum+'.csv']);
+    }
+
+$("#cover-btn").on("click", function(){
+  start();
+});
+
+$("#form-cover-btn").on("click",function(){
+  ready();
+});
+
+$("#submit-btn").on("click", function(){
+  submit();
+});
+
+$("#thanks-btn").on("click", function(){
+  for(var i = 0; i < part.length; i++){
+    if (i < 1) {
+      participantInfo.push("Age: " + part[i].value);
+    } else {
+      participantInfo.push(part[1].value);
+    }
+  }
+  participantNum = "C" + (Math.floor(Math.random()*90000) + 10000)
+  participantInfo.push(participantNum);
+  exportFile();
+});
