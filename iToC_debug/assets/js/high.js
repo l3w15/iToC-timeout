1b45468614ee190fd3cd8d5691e5711686aca79b
Lewis Watson <lewwwiswatson@gmail.com>
2018-04-25 11:34:24 +0100

    :recycle: Refactor #exportFile to be more likely to display correctly

--- iToC_debug/assets/js/high.js
+++ iToC_debug/assets/js/high.js
@@ -123,9 +123,9 @@ function submit() {
 
 function exportFile()
     {
-        var data = [unsortedRecord, thinkTime, allMovesRecord, participantInfo];
+        var data = [allMovesRecord, unsortedRecord, thinkTime, participantInfo];
         //var data = [[1,2,3,4,5],[11,22,33,44,55],[111,222,333,444,555],[1111,2222,3333,4444,5555]];
-        var keys = ['"Answers submitted"', '"Thinking Time (seconds)"', '"Dragged moves"', '"Participant Info"'];
+        var keys = ['"Dragged moves"', '"Answers submitted"', '"Thinking Time (seconds)"', '"Participant Info"'];
 
         var convertToCSV = function(data, keys) {
             var orderedData = [];
